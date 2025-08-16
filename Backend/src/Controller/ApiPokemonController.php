be788d810961f46945c9b796124625058d85cdb3
emery-fokus <fokwangemery@icloud.com>
2025-08-16 18:14:43 +0200

    creaction d'un controller avec la methode post pour importer les donnes des pokemons dans la base de donnees

--- Backend/src/Controller/ApiPokemonController.php
+++ Backend/src/Controller/ApiPokemonController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use App\Repository\PoketypeRepository;
+use App\Repository\PoketypeRepository; // 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
